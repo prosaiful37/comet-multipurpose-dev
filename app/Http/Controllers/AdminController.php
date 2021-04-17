289f8528fb648d7b836a6485cd756c03c0aff6c1
saiful islam <saiful01636758237@gmail.com>
2021-04-17 12:21:57 +0600

    admin dashboard template load

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * show admin login form
+     */
+
+     public function showAdminLoginForm(){
+         return view('admin.login');
+     }
+
+
+
+     /**
+     * show admin register form
+     */
+
+    public function showAdminRegisterForm(){
+        return view('admin.register');
+    }
+
+    /**
+    * show admin dashboard
+    */
+
+   public function showAdminDashboard(){
+       return view('admin.dashboard');
+   }
+
+
+
+
+}
