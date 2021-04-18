f197b3efac30194257e4cac36821c2525f5dd35b
saiful islam <saiful01636758237@gmail.com>
2021-04-18 11:11:10 +0600

    admin login features to admin login template

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//admin tamplate load
 Route::get('/admin/login', [App\Http\Controllers\AdminController::class, 'showAdminLoginForm']) -> name('admin.login');
 Route::get('/admin/register', [App\Http\Controllers\AdminController::class, 'showAdminRegisterForm']) -> name('admin.register');
 Route::get('/admin/dashboard', [App\Http\Controllers\AdminController::class, 'showAdminDashboard']) -> name('admin.dashboard');
+Route::POST('/admin/login', [App\Http\Controllers\Auth\LoginController::class, 'login']) -> name('admin.login');
